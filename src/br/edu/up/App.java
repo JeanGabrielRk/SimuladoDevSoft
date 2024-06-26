2ca98c1960eee8138e642f2b5b2ce08498d353bb
Auto Logon <autologon@CSED.LAB>
2024-06-26 10:04:53 -0300

    Programa Completo

--- src/br/edu/up/App.java
+++ src/br/edu/up/App.java
@@ -1,5 +1,9 @@
+package br.edu.up;
+
+import br.edu.up.telas.Interface;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Interface.mostrarMenu();
     }
 }
